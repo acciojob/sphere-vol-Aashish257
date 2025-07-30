11f376943a07b5b2ef1414e470bc45ff801bd56a
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-07-30 17:10:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	event.preventDefault(); 
+
+	const radiusValue = parseFloat(document.getElementById("radius").value);
+
+	if (radiusValue < 0 || isNaN(radiusValue)) {
+		return false;
+	}
+
+	const volume = (4 / 3) * Math.PI * Math.pow(radiusValue, 3);
+
+	document.getElementById("volume").value = volume.toFixed(4); 
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
